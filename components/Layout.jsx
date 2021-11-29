3a3b51b7bc15aa83360d20ed3f0de1cd9f109f7e
IvanDanyliuk <ivandaniliuk@gmail.com>
2021-11-29 16:39:39 +0200

    Created product cards

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Head from 'next/head'
-import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+    const classes = useStyles();
     return (
         <div>
             <Head>
                 <title>Next E-Store</title>
             </Head>
-            <AppBar position='static'>
+            <AppBar position='static' className={classes.navbar}>
                 <Toolbar>
                     <Typography>e-store</Typography>
                 </Toolbar>
             </AppBar>
-            <Container>
+            <Container className={classes.main}>
                 {children}
             </Container>
-            <footer>
+            <footer className={classes.footer}>
                 <Typography>
                     All rights reserved. E-Store. 2021
                 </Typography>
